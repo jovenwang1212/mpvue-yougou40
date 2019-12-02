4bdaad12bb63dc7ff20b060749db1633d45a3ef4
王侨文 <wangqiaowen@itcast.cn>
2019-12-02 10:02:42 +0800

    ok

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -14,28 +14,30 @@
       indicator-color="rgba(255,255,255,0.3)"
       indicator-active-color="#fff"
     >
-      <block v-for="(item, index) in 3" :key="index">
+      <block v-for="(item, index) in swiperdata" :key="index">
         <swiper-item>
-          <image src="https://api.zbztb.cn/pyg/banner2.png"></image>
+          <image :src="item.image_src"></image>
         </swiper-item>
       </block>
     </swiper>
 
     <!-- 分类 -->
     <ul class="cates">
-      <li v-for="(item, index) in 4" :key="index">
-        <img src="https://api.zbztb.cn/pyg/icon_index_nav_2@2x.png" alt="">
+      <li v-for="(item, index) in catitems" :key="index">
+        <img :src="item.image_src" alt="" />
       </li>
     </ul>
 
     <!-- 楼层 -->
     <ul>
-      <li class="floor-item" v-for="(item2, index2) in 3" :key="index2">
-        <img src="https://api.zbztb.cn/pyg/pic_floor01_title.png" alt="">
+      <li class="floor-item" v-for="(floor, fIndex) in floordata" :key="fIndex">
+        <img :src="floor.floor_title.image_src" alt="" />
         <div class="products">
-          <img src="https://api.zbztb.cn/pyg/pic_floor01_1@2x.png" alt="">
+          <img :src="floor.product_list[0].image_src" alt="" />
           <div class="right">
-            <img v-for="(item, index) in 4" :key="index" src="https://api.zbztb.cn/pyg/pic_floor01_1@2x.png" alt="">
+            <block v-for="(item, index) in floor.product_list" :key="index">
+              <img v-if="index" :src="item.image_src" alt="" />
+            </block>
           </div>
         </div>
       </li>
@@ -44,7 +46,44 @@
 </template>
 
 <script>
-export default {}
+
+export default {
+  data () {
+    return {
+      swiperdata: [],
+      catitems: [],
+      floordata: []
+    }
+  },
+  created () {
+    this.getSwiperdata()
+    this.getCatitems()
+    this.getFloordata()
+  },
+  methods: {
+    getSwiperdata () {
+      this.$request({
+        url: '/api/public/v1/home/swiperdata'
+      }).then(data => {
+        this.swiperdata = data
+      })
+    },
+    getCatitems () {
+      this.$request({
+        url: '/api/public/v1/home/catitems'
+      }).then(data => {
+        this.catitems = data
+      })
+    },
+    getFloordata () {
+      this.$request({
+        url: '/api/public/v1/home/floordata'
+      }).then(data => {
+        this.floordata = data
+      })
+    }
+  }
+}
 </script>
 
 <style lang="less">
@@ -77,39 +116,39 @@ swiper image {
   height: 340rpx;
 }
 
-.cates{
+.cates {
   height: 194rpx;
   display: flex;
   align-items: center;
   justify-content: space-evenly;
-  img{
+  img {
     width: 128rpx;
     height: 140rpx;
   }
 }
 
-.floor-item{
-  margin-bottom:20rpx;
-  >img{
+.floor-item {
+  margin-bottom: 20rpx;
+  > img {
     width: 100%;
     height: 88rpx;
   }
-  .products{
+  .products {
     display: flex;
-    margin-top:20rpx;
-    padding-left:18rpx;
-    >img{
+    margin-top: 20rpx;
+    padding-left: 18rpx;
+    > img {
       width: 232rpx;
       height: 386rpx;
     }
-    .right{
-      flex:1;
+    .right {
+      flex: 1;
       font-size: 0;
-      >img{
+      > img {
         width: 232rpx;
         height: 188rpx;
-        margin-left:10rpx;
-        margin-bottom:10rpx;
+        margin-left: 10rpx;
+        margin-bottom: 10rpx;
       }
     }
   }
---
+++ src/utils/request.js
@@ -0,0 +1,35 @@
+const BASE_URL = 'https://ugo.botue.com'
+function request (options) {
+  // 开启loading
+  if (!options.noLoading) {
+    wx.showLoading({
+      title: '加载中...',
+      mask: true
+    })
+  }
+  return new Promise((resolve, reject) => {
+    wx.request({
+      url: BASE_URL + options.url,
+      success: res => {
+        let { meta, message } = res.data
+        if (meta.status === 200) {
+          // 成功的消息
+          resolve(message)
+        } else {
+
+        }
+      },
+      fail: err => {
+        console.log(err)
+      },
+      complete () {
+        // 关闭loading
+        if (!options.noLoading) {
+          wx.hideLoading()
+        }
+      }
+    })
+  })
+}
+
+export default request
